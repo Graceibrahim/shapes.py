cfa8fc839c57ff95fb1154d3f6d3959d0b905e00
Graceibrahim <47751148+Graceibrahim@users.noreply.github.com>
2019-05-02 19:12:23 +0300

    Add files via upload

---
+++ shapes.py/shapes.py
@@ -0,0 +1,49 @@
+class circle:
+	def __init__(self, r):
+		self.r =r
+
+	def area(self, A):
+			A=(3.142*r**2)
+			return A
+			
+	
+	def circumference(self,  c):
+		c=2(3.142*r)
+		return c
+		
+class square:
+	def __init__ (self, a):
+		self.a=a
+
+	def area(self, A):
+		A=a**2
+		return A
+	def perimeter(self, P):
+		P=4*a
+
+class rectangle:
+	def __init__ (self, w,I):
+		self.w=w
+		self.I=I
+
+	def area(self, A):
+		A=w*I
+		return A
+
+	def perimeter(self,P):
+		P=2(I+w)
+		return P
+
+class sphere:
+	def __init__ (self, r):
+		self.r=r
+
+	def area(self, A):
+		A=4*3.142*r**2
+		return A
+
+	def volume(self,V):
+		v=4/3(3.142*r**3)
+
+
+
